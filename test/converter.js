b1a7c3eac9fd7a90e5dd8adbabf9ce9391157cfe
mike <mike@web-dev.ca>
2021-08-19 21:32:57 -0600

    tests

---
+++ test/converter.js
@@ -0,0 +1,18 @@
+// test/converter.js
+
+var expect    = require("chai").expect;
+// var converter = require("../app/converter");
+
+describe("Color Code Converter", function() {
+    describe("RGB to Hex conversion", function() {
+        it("converts the basic colors", function() {
+            expect(1+1).to.equal(2)
+        });
+    });
+
+    describe("Hex to RGB conversion", function() {
+        it("converts the basic colors", function() {
+            expect(1+1).to.equal(2)
+        });
+    });
+});
